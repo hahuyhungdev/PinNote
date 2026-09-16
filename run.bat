ceeecf80f66615857cbe90e13c714a6ceccc129d
hahuyhungdev <hahuyhungdev@gmail.com>
2026-09-16 12:02:18 +0700

    feat(build): add electron-builder packaging for portable exe, nsis installer, and win-unpacked

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
 cd /d "%~dp0"
-start "" "%~dp0node_modules\electron\dist\electron.exe" "%~dp0."
+if exist "%~dp0dist\win-unpacked\PinNote.exe" (
+  start "" "%~dp0dist\win-unpacked\PinNote.exe"
+) else (
+  start "" "%~dp0node_modules\electron\dist\electron.exe" "%~dp0."
+)
